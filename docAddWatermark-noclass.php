7a994c97f4a5a3d66b89d6a5ab53040000737899
VamosCathy <yl.pan.pro@gmail.com>
2015-05-08 10:44:42 +0800

    testing new file

---
+++ docAddWatermark-noclass.php
@@ -0,0 +1,10 @@
+<?php
+//convert word to pdf
+function convertDocToPdf($originFilePath,$outputDirPath){
+	$command = 'unoconv --format %s --output %s %s';
+	$command = sprintf($command,'pdf',$outputDirPath,$originFilePath);
+	system($command,$output);
+
+	return $output;
+}
+?>
